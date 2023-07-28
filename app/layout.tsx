90e09820f0f64b8eb460314e114ba7c0a125b905
Dmitry G. Anderson <dandgerson@gmail.com>
2023-07-28 14:08:21 +0300

    feat: set adaptive flow

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={clsx(inter.className, s.body)}>
+      <body className={clsx(inter.className)}>
         <header
           className={s.header}
           style={{
@@ -57,72 +57,70 @@ export default function RootLayout({
           />
         </header>
 
-        {children}
+        <div className={s.content}>{children}</div>
 
         <footer className={clsx(s.footer)}>
-          <div className={clsx(s.content)}>
+          <div
+            style={{
+              display: 'flex',
+            }}
+          >
             <div
+              className={s.footer_item}
               style={{
-                display: 'flex',
+                flex: 1,
               }}
             >
-              <div
-                className={s.content_item}
-                style={{
-                  flex: 1,
-                }}
-              >
-                <div className={s.geoLink}>
-                  <Image
-                    src="/images/icon-arrow.svg"
-                    alt="icon arrow"
-                    className={s.geoLink_icon}
-                    width={15}
-                    height={15}
-                    priority
-                  />
-                  <div className={s.geoLink_label}>Москва</div>
-                </div>
-
-                <div>
-                  <div>+7 495 000 00 00 — Бизнесу и ИП</div>
-                  <div>+7 495 111 11 11 — Частным клиентам</div>
-                </div>
-
-                <div className={s.social}>
-                  <Image
-                    src="/images/icon-telegram.svg"
-                    alt="icon telegram"
-                    width={24}
-                    height={24}
-                    priority
-                  />
-                </div>
+              <div className={s.geoLink}>
+                <Image
+                  src="/images/icon-arrow.svg"
+                  alt="icon arrow"
+                  className={s.geoLink_icon}
+                  width={15}
+                  height={15}
+                  priority
+                />
+                <div className={s.geoLink_label}>Москва</div>
               </div>
-              <div
-                style={{
-                  flex: 1,
-                }}
-              ></div>
-            </div>
 
-            <div className={s.separator} />
-            <nav className={clsx(s.content_item, s.navbar)}>
-              <Navbar
-                s={s}
-                additionalItems={[
-                  {
-                    path: '/try-free',
-                    label: 'Получить годовую лицензию',
-                  },
-                ]}
-              />
-            </nav>
+              <div>
+                <div>+7 495 000 00 00 — Бизнесу и ИП</div>
+                <div>+7 495 111 11 11 — Частным клиентам</div>
+              </div>
 
-            <div className={s.content_item}>
-              © 2021-2023.ООО «ЧТО ТО», официальный сайт in2sql. Передавая нам
-              свои контактные данные, мы гарантируем безопасность их хранения.
+              <div className={s.social}>
+                <Image
+                  src="/images/icon-telegram.svg"
+                  alt="icon telegram"
+                  width={24}
+                  height={24}
+                  priority
+                />
+              </div>
             </div>
+            <div
+              style={{
+                flex: 1,
+              }}
+            ></div>
+          </div>
+
+          <div className={s.separator} />
+          <nav className={clsx(s.content_item, s.navbar)}>
+            <Navbar
+              s={s}
+              additionalItems={[
+                {
+                  path: '/try-free',
+                  label: 'Получить годовую лицензию',
+                },
+              ]}
+            />
+          </nav>
+
+          <div className={s.content_item}>
+            © 2021-2023.ООО «ЧТО ТО», официальный сайт in2sql. Передавая нам
+            свои контактные данные, мы гарантируем безопасность их хранения.
           </div>
         </footer>
       </body>
