24e8cb1c2d73161c0d77b53c39794a4e2de27880
Sergey Komarov <komarovs33@mail.ru>
2023-08-11 17:35:48 +0300

    fix: map

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -81,7 +81,7 @@ const Contacts = () => {
             <br /> ОГРН 1205000100997
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d9066.216032739287!2d37.58841528653955!3d55.38323906719255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1zMTQyMTgxLCDQnNC-0YHQutC-0LLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwg0LPQvtGA0L7QtCDQn9C-0LTQvtC70YzRgdC6LCDQtNC10YDQtdCy0L3RjyDQkdC10YDQtdC20LrQuCwg0YLQtdGALiDQmtCw0YHQutCw0LQg0J_QsNGA0LosINC0LiA0Mywg0LrQsi4gMw!5e0!3m2!1sru!2sru!4v1691763637528!5m2!1sru!2sru"
-              width="482"
+              width="375"
               height="420"
               style={{ border: '0' }}
               loading="lazy"
